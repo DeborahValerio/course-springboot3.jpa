4857cc98d3be901cc4f8a58d5506d49c4af77277
DeborahValerio <dborah.v@gmail.com>
2023-03-30 16:58:20 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/debproject/course/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.debproject.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.debproject.course.entities.Order;
+import com.debproject.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
